1a0fbf70778201f648c1da8a52081f6350859f1a
Justin Purrington <justin@eipimo.co.uk>
2021-01-17 22:47:46 +0000

    Chord identification exercise.

--- note_identification.py
+++ note_identification.py
@@ -3,10 +3,18 @@ import argparse
 
 from random import randint
 from time import sleep
+from collections import namedtuple
+
+
+ChordShape = namedtuple('ChordShape', (
+    'root_string',
+    'first_root',
+    'chord',
+))
 
 
 DEFAULT_NUMBER_OF_TURNS = 20
-DEFAULT_TIME_BETWEEN_TURNS = 3
+DEFAULT_TIME_BETWEEN_TURNS = 10
 DEFAULT_TIME_BEFORE_REVEAL = 2
 DEFAULT_EXERCISE='fret'
 MAX_FRET = 17
@@ -33,6 +41,59 @@ GUITAR_STRINGS = {
     3: 'G',
     2: 'B',
     1: 'E',
+}
+
+CHORD_SHAPES = {
+    ('C', 'Maj'): ChordShape(
+        root_string=5,
+        first_root=3,
+        chord=(None, 3, 2, 0, 1, 0)
+    ),
+    ('C', 'min'): ChordShape(
+        root_string=5,
+        first_root=3,
+        chord=(None, 3, 1, 0, 1, None)
+    ),
+    ('A', 'Maj'): ChordShape(
+        root_string=5,
+        first_root=0,
+        chord=(None, 0, 2, 2, 2, 0)
+    ),
+    ('A', 'min'): ChordShape(
+        root_string=5,
+        first_root=0,
+        chord=(None, 0, 2, 2, 1, 0)
+    ),
+    ('G', 'Maj'): ChordShape(
+        root_string=6,
+        first_root=3,
+        chord=(3, 2, 0, 0, 0, 3)
+    ),
+    ('G', 'min'): ChordShape(
+        root_string=6,
+        first_root=3,
+        chord=(3, 1, 0, 0, None, None)
+    ),
+    ('E', 'Maj'): ChordShape(
+        root_string=6,
+        first_root=0,
+        chord=(0, 2, 2, 1, 0, 0)
+    ),
+    ('E', 'min'): ChordShape(
+        root_string=6,
+        first_root=0,
+        chord=(0, 2, 2, 0, 0, 0)
+    ),
+    ('D', 'Maj'): ChordShape(
+        root_string=4,
+        first_root=0,
+        chord=(None, None, 0, 2, 3, 2)
+    ),
+    ('D', 'min'): ChordShape(
+        root_string=4,
+        first_root=0,
+        chord=(None, None, 0, 2, 3, 1)
+    ),
 }
 
 
@@ -76,16 +137,16 @@ def main():
         '--chord-shapes',
         type=list_of_strings,
         required=False,
-        default=['A', 'E'],
+        default=['C', 'A', 'G', 'E', 'D'],
     )
 
     parser.add_argument(
         '--chords',
         type=list_of_strings,
         required=False,
-        default=['M', 'm'],
+        default=['Maj', 'min'],
     )
-    
+
     args = parser.parse_args()
     run_exercise(args)
 
@@ -118,7 +179,7 @@ def half_step_c_interval(note):
             if note == enharmonic:
                 return interval
         interval += 1
-    
+
     raise ValueError(f'Unknown note: {note}')
 
 
@@ -136,19 +197,48 @@ def format_fret(string_number, fret):
     string_name = format_string(string_number)
     return f'{string_name} fret {fret}'
 
+
 def format_string(string_number):
     string_note = GUITAR_STRINGS[string_number]
     return f'{string_number}{string_note}'
 
 
-def print_fret_tab(string_number, fret):
-    for num, name in sorted(list(GUITAR_STRINGS.items())):
-        if num == string_number:
-            print(f'{name} -{fret}--')
-        else:
-            padding = '-' if fret < 10 else '--'
-            print(f'{name} -{padding}--')
+def note_interval(note, interval):
+    new_offset = (half_step_c_interval(note) + interval) % 12
+    return NOTES[new_offset][0]
 
+
+def chord_form(note, chord_shape):
+    root_note = note_interval(
+        GUITAR_STRINGS[chord_shape.root_string],
+        chord_shape.first_root
+    )
+
+    root = half_step_c_interval(root_note)
+    fretted = half_step_c_interval(note)
+    offset = (fretted - root) % 12
+
+    return tuple(
+        n + offset if n is not None else None
+        for n in chord_shape.chord
+    )
+
+
+def print_single_note_tab(string_number, fret):
+    frets = [None] * 6
+    frets[6 - string_number] = fret
+    print_fret_tab(frets)
+
+def print_fret_tab(frets):
+    for (num, name), fret in zip(sorted(list(GUITAR_STRINGS.items())), reversed(frets)):
+        if fret is None:
+            padded = '--'
+        elif fret < 10:
+            padded = f'-{fret}'
+        else:
+            padded = fret
+
+        print(f'{name} -{padded}--')
 
 def run_exercise(args):
     exercise_class = exercises[args.exercise]
@@ -164,9 +254,6 @@ def run_exercise(args):
 
 
 class Exercise:
-    def __init__(self, args):
-        self.args = args
-    
     def print_question(self):
         raise NotImplementedError()
 
@@ -176,18 +263,17 @@ class Exercise:
 
 class FretExercise(Exercise):
     def __init__(self, args):
-        super().__init__(args)
         self.string_number = pick_random(list(GUITAR_STRINGS.keys()))
 
-        if self.args.frets:
-            self.fret = pick_random(self.args.frets)
+        if args.frets:
+            self.fret = pick_random(args.frets)
         else:
             self.fret = randint(0, MAX_FRET)
 
     def print_question(self):
         print(format_fret(self.string_number, self.fret))
-        print_fret_tab(self.string_number, self.fret)
-    
+        print_single_note_tab(self.string_number, self.fret)
+
     def print_solution(self):
         note = fret_to_note(self.string_number, self.fret)
         print(f'The note is {note}')
@@ -195,7 +281,6 @@ class FretExercise(Exercise):
 
 class NoteExercise(Exercise):
     def __init__(self, args):
-        super().__init__(args)
         self.string_number = pick_random(list(GUITAR_STRINGS.keys()))
         self.string_note = GUITAR_STRINGS[self.string_number]
         self.string_name = format_string(self.string_number)
@@ -204,17 +289,34 @@ class NoteExercise(Exercise):
 
     def print_question(self):
         print(f'Play {self.note} on the {self.string_name} string')
-    
+
     def print_solution(self):
         open_string = half_step_c_interval(self.string_note)
         fretted = half_step_c_interval(self.note)
         fret = (fretted - open_string) % 12
         print(format_fret(self.string_number, fret))
-        print_fret_tab(self.string_number, fret)
-     
+        print_single_note_tab(self.string_number, fret)
+
+
+class ChordExercise(Exercise):
+    def __init__(self, args):
+        self.chord_shape_root_note = pick_random(args.chord_shapes)
+        self.chord = pick_random(args.chords)
+        self.chord_shape = CHORD_SHAPES[(self.chord_shape_root_note, self.chord)]
+        note = pick_random(NOTES)
+        self.note = pick_random(note)
+
+    def print_question(self):
+        print(f'Play {self.note} {self.chord} using the {self.chord_shape_root_note} shape')
+
+    def print_solution(self):
+        chord = chord_form(self.note, self.chord_shape)
+        print_fret_tab(chord)
+
 
 exercises = {
     'fret': FretExercise,
     'note': NoteExercise,
+    'chord': ChordExercise,
 }
 main()
